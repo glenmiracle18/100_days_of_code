f257fa13d365ef6d20bcaef17f9fdba59c51467a
glenmiracle18 <m.bonyu@alustudent.com>
2023-11-08 21:38:03 +0200

    done

--- just some js/talent_app/frontend.js
+++
@@ -1,5 +0,0 @@
-const button = document.querySelector('button')
-const container = document.querySelector('.container')
-const h2 = document.querySelector('h2')
-const section = document.querySelector('section')
-
